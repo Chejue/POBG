e7f40270584c5c57045ad146741c679bff591f16
Chejue <83407724+Chejue@users.noreply.github.com>
2021-06-16 20:32:13 +0800

    Delete Resources/map directory

--- Resources/map/pobg装饰.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="pobg装饰" tilewidth="32" tileheight="32" tilecount="176" columns="8">
- <image source="pobg装饰.png" width="256" height="704"/>
-</tileset>
--- Resources/map/沙漠01.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="沙漠01" tilewidth="32" tileheight="32" tilecount="152" columns="8">
- <image source="沙漠01.png" width="256" height="608"/>
-</tileset>
